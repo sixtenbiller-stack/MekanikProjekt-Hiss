6abbdd7fa5250710de068ab4729f058bbcc73a2a
sixtenbiller-stack <sixtenbiller@gmail.com>
2026-04-13 13:57:08 +0200

    Fixade filnamn igen

--- solver_controller.m
+++
@@ -1,6 +0,0 @@
-function dydt = solver(y0, bromsKraft, trumRadie)
-    %Initierar startvärden
-    h = y0(1); %Höjd (för själva hissen)
-    v = y0(2); %Hastighet (positivt antingen upp eller ner, vi får bestämma oss)
-    varme = y0(3); %Värmeenergiutveckling under processen
-end
